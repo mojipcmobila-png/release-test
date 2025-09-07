8fc47e2abb1c326f1a48033c4745f5f6b280f16e
shaadcode <shaadcode@gmail.com>
2025-09-07 21:22:26 +0330

    feat: add emoji for semantic-release

--- release.config.js
+++ release.config.js
@@ -10,7 +10,24 @@ module.exports = {
     ],
     plugins: [
         '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                presetConfig: {
+                    types: [
+                        { type: 'feat', section: '🚀 Features', hidden: false },
+                        { type: 'fix', section: '🐞 Bug Fixes', hidden: false },
+                        { type: 'chore', section: '🧹 Chores', hidden: false },
+                        { type: 'docs', section: '📚 Documentation', hidden: false },
+                        { type: 'refactor', section: '🔧 Refactoring', hidden: false },
+                        { type: 'perf', section: '⚡ Performance', hidden: false },
+                        { type: 'test', section: '✅ Tests', hidden: false },
+                        { type: 'style', section: '🎨 Styles', hidden: false }
+                    ]
+                }
+            }
+        ],
         '@semantic-release/changelog',
         '@semantic-release/npm',
         ['@semantic-release/git', {
