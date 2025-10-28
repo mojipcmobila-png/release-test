dd7e01bc1abb3037efe120784aaef0785737fdc1
shaadcode <shaadcode@gmail.com>
2025-10-28 17:13:51 +0330

    feat: update

--- .release-it.ts
+++ .release-it.ts
@@ -12,60 +12,59 @@ export default {
   github: {
     "release": true,
     "releaseName": "v${version}",
-    changelog: "CHANGELOG.md"
   },
   "npm": {
     "publish": true
   },
-  // "plugins": {
-  //   "@release-it/conventional-changelog": {
-  //     "infile": "CHANGELOG.md",
-  //     "header": "# Changelog",
-  //     "preset": {
-  //       "name": "conventionalcommits",
-  //       "types": [
-  //         {
-  //           "type": "feat",
-  //           "section": "🚀 Features",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "fix",
-  //           "section": "🐞 Bug Fixes",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "chore",
-  //           "section": "🧹 Chores",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "docs",
-  //           "section": "📚 Documentation",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "refactor",
-  //           "section": "🔧 Refactoring",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "perf",
-  //           "section": "⚡ Performance",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "test",
-  //           "section": "✅ Tests",
-  //           "hidden": false
-  //         },
-  //         {
-  //           "type": "style",
-  //           "section": "🎨 Styles",
-  //           "hidden": false
-  //         }
-  //       ]
-  //     }
-  //   }
-  // }
+  "plugins": {
+    "@release-it/conventional-changelog": {
+      "infile": "CHANGELOG.md",
+      "header": "# Changelog",
+      "preset": {
+        "name": "conventionalcommits",
+        "types": [
+          {
+            "type": "feat",
+            "section": "🚀 Features",
+            "hidden": false
+          },
+          {
+            "type": "fix",
+            "section": "🐞 Bug Fixes",
+            "hidden": false
+          },
+          {
+            "type": "chore",
+            "section": "🧹 Chores",
+            "hidden": false
+          },
+          {
+            "type": "docs",
+            "section": "📚 Documentation",
+            "hidden": false
+          },
+          {
+            "type": "refactor",
+            "section": "🔧 Refactoring",
+            "hidden": false
+          },
+          {
+            "type": "perf",
+            "section": "⚡ Performance",
+            "hidden": false
+          },
+          {
+            "type": "test",
+            "section": "✅ Tests",
+            "hidden": false
+          },
+          {
+            "type": "style",
+            "section": "🎨 Styles",
+            "hidden": false
+          }
+        ]
+      }
+    }
+  }
 } satisfies Config;
